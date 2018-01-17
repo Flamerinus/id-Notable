6bdd1828106bf2a6f24fd2224341b32b2d498f2c
Flamerinus <alexmosuyevon@yahoo.es>
2018-01-17 03:18:35 +0100

    a2-b2 done

--- sketch-a+b2.js
+++ sketch-a+b2.js
@@ -51,10 +51,10 @@ A<sup>2</sup>
 B<sup>2</sup>
 </td>`;
 var descriptaminusb2 = `<td>
-(A - B)<sup>2</sup> = (<span style="color:#f9bd03">A</span> - <span style="color:#e34a06">B</span>) x (<span style="color:#407026">A</span> - <span style="color:#31cce8">B</span>) =
+(A - B)<sup>2</sup> = (<span style="color:#e34a06">A</span> - <span style="color:#f9bd03">B</span>) x (<span style="color:#407026">A</span> - <span style="color:#31cce8">B</span>) =
 </td>
-<td>
-<span style="color:#e34a06">A<sup>2</sup></span>
+<td style="border: 3px solid; border-color: #e34a06;">
+<span style="color:#e34a06;">A<sup>2</sup></span>
 </td>
 <td>
  -
@@ -68,6 +68,14 @@ var descriptaminusb2 = `<td>
 <td style="background-color:#31cce8">
 B<sup>2</sup>
 </td>`;
+var descripta2b2 = `<td style="border: 3px solid; border-color: #e34a06;">
+<span style="color:#e34a06">A<sup>2</sup></span></td> <td>-</td>
+<td style="border: 3px solid; border-color:#31cce8">B<sup>2</sup>
+</td>
+<td> = (<span style="color:#f9bd03">A</span> + <span style="color:#e34a06">B</span>) x (<span style="color:#407026">A</span> - <span style="color:#31cce8">B</span>)
+</td>
+`;
+
 var abcol="", a2col="", b2col="", botabcol="", atopcol="", aleftcol="", btopcol="", bleftcol="";
 
 /*function that does things once, in setup(), so that they don't have to be
@@ -83,6 +91,11 @@ function initial(s){
 		case 2:
 			document.getElementById("descript").innerHTML = descriptaminusb2;
 			document.getElementById("rangeA").innerHTML = `25 <input type="range" id="A" min="25" max="35" value="30">35`;
+			document.getElementById("rangeB").innerHTML = `6 <input type="range" id="B" min="6" max="10" value="8">10`;
+			break;
+		case 3:
+			document.getElementById("descript").innerHTML = descripta2b2;
+			document.getElementById("rangeA").innerHTML = `24 <input type="range" id="A" min="24" max="35" value="30">35`;
 			document.getElementById("rangeB").innerHTML = `6 <input type="range" id="B" min="6" max="10" value="8">10`;
 			break;
 	}
@@ -205,11 +218,11 @@ function amasb2() {
 			textSize(25);
 		  //top A and B
 			textAlign(CENTER, TOP);
-			fill(btopcol);
+			fill(atopcol);
 			text("B", aplus - b / 2, y);
 
 			textAlign(CENTER, BOTTOM);
-			fill(atopcol);
+			fill(btopcol);
 			text("A", x + a / 2, y);
 			//Left A and B
 			textAlign(RIGHT, CENTER);
@@ -230,6 +243,146 @@ function amasb2() {
 			text("B", aplus + 3, bplus - b / 2);
 			//Left inner 22
 			text(aminusb, x + 3, y + (a-b) / 2);
+
+			break;
+		case 3:
+			resultaplusb = ((a * a) - (b * b))/100;
+			document.getElementById("row2").innerHTML = "A<sup>2</sup>  - B<sup>2</sup> = " + a/10+ "<sup>2</sup>" + " - " + b/10 + "<sup>2</sup> = " + (a*a)/100 + " - " + (b*b)/100 + " = " + resultaplusb;
+			x = (canvwidth - (a + b))/2;
+			y = (canvheight - a)/2;
+			aplus = a + x;
+			bplus = a + y;
+			//The rectangles for a^2-b^2
+
+			stroke(0);
+			strokeWeight(3);
+			fill(abcol);
+			rect(aplus, y, b, a);
+			rect(x, bplus-b, a, b);
+
+			stroke(b2col);
+			noFill();
+			rect(aplus-b,y, b, b);
+
+			stroke(a2col);
+			strokeWeight(3);
+			noFill();
+			rect(x, y, a, a);
+
+			//Chars for a^2-b^2
+			fill(a2col);
+			noStroke();
+			textAlign(CENTER, CENTER);
+			//Big interior characters
+			textSize(40);
+			text("A", x + a / 2, y + a / 2);
+			textSize(20);
+			text("2", x + 15 + a / 2, y - 15 + a / 2);
+
+			fill(b2col);
+			textSize(40);
+			text("B", aplus-b + b / 2, y + b / 2);
+			textSize(20);
+			text("2", 15 + aplus - b / 2, y + b / 2 - 15);
+
+			fill(0);
+			textSize(40);
+			text("B", aplus + b / 2,bplus - b / 2);
+			textSize(20);
+			text("2", 15 + aplus + b / 2, bplus - b / 2 - 15);
+
+			stroke(0);
+			line(aplus, bplus -b, aplus+b, bplus-b);
+			noStroke();
+
+			fill(0);
+			textSize(40);
+			text("AB", aplus + b / 2, y + a / 2);
+			textSize(40);
+			text("AB", x + a / 2, bplus - b / 2);
+
+				//Smaller outer characters
+			textSize(25);
+			//top A and B
+			textAlign(CENTER, TOP);
+			fill(atopcol);
+			text("A", x + a / 2, y);
+			fill(btopcol);
+			text("B", aplus + b / 2, y);
+			textAlign(CENTER, BOTTOM);
+
+			fill(0);
+			text("A + B", x + (a + b) / 2, y - 10);
+			stroke(0);
+			line(x, y-10, aplus+b, y-10);
+			noStroke();
+
+			//Left A and B
+			textAlign(RIGHT, CENTER);
+			fill(aleftcol);
+			text("A", x-3, y + a / 2);
+			textAlign(LEFT, CENTER);
+			fill(bleftcol);
+			text("B", x+3, bplus - b / 2);
+
+			fill(0);
+			text("A-B",x + 15, y+(a-b)/2);
+
+			stroke(0);
+			line(x+10, y+5, x+10, bplus-b-5);
+			noStroke();
+
+			fill(0);
+			//Bottom A and B
+			textAlign(CENTER, TOP);
+			text("A", x + a / 2, bplus);
+			text("B", aplus + b / 2, bplus);
+
+			//Right A
+			textAlign(LEFT, CENTER);
+			text("A", aplus + b / 1 + 3, y + a / 2);
+
+			//right illustrations
+
+			fill(a2col);
+			rect(aplus+b + 30, y, 100,100);
+
+			textAlign(CENTER, BOTTOM);
+			fill(0);
+			textSize(20);
+			text("A - B",aplus+b+30 + 130/2, y);
+			textSize(10);
+			text("2         2", aplus+b+30+130/2+7, y-15);
+
+			fill(b2col);
+			rect(aplus+b + 30 + 100 -30, y, 30,30);
+
+			textAlign(CENTER, BOTTOM);
+			fill(0);
+			textSize(20);
+			text("(A + B)x(A - B)",aplus+b+30 + 130/2, bplus -100);
+
+			stroke(0);
+			strokeWeight(1);
+			line(aplus+b+30, y+70, aplus+b+30+100+30,y+70);
+			line(aplus+b+30+100, y, aplus+b+30+100+30,y);
+			line(aplus+b+30+100, y+100-1, aplus+b+30+100+30,y+100-1);
+			line(aplus+b+30+100+30, y, aplus+b+30+100+30,y+100-1);
+
+			noStroke();
+
+			fill(a2col);
+			rect(aplus+b + 30, bplus-100, 100+30,100-30);
+
+			stroke(0);
+			strokeWeight(1);
+			line(aplus+b+30, bplus, aplus+b+30+100+30-1,bplus);
+			line(aplus+b+30, bplus-30, aplus+b+30+100+30-1,bplus-30);
+			line(aplus+b+30, bplus, aplus+b+30,bplus-30);
+			line(aplus+b+30+100+30-1, bplus, aplus+b+30+100+30-1,bplus-30);
+			line(aplus+b+30+100, bplus, aplus+b+30+100,bplus-100);
+			noStroke();
+
 
 			break;
   }
